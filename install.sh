72de4b9dadcc50991cb4ddf0a4d420140a84dcfc
Dan Habot <dan.habot@gmail.com>
2019-03-16 14:04:59 -0400

    Made install script more efficient

--- install.sh
+++ install.sh
@@ -7,16 +7,22 @@ yes | cp -rf ./variety $HOME/.config
 yes | cp -rf ./gtk* $HOME/.config
 
 echo "Adding PPAs"
-for ppa in $(cat ppa.list);
+apt-cache policy | grep http | awk '{print $2 $3}' | sort -u > /tmp/ppa.list
+
+for ppa in $(diff -u /tmp/ppa.list ppa.list | grep -Po "(?<=\+)http.*");
 do
+	echo "### Adding $ppa ###"
 	sudo add-apt-repository $ppa -y
 done
 
 sudo apt update
 
 echo "Installing packages"
-for package in $(cat packages.list);
+apt-mark showmanual > /tmp/packages.list
+
+for package in $(diff -u /tmp/packages.list packages.list | grep -Po "(?<=\+)[a-zA-Z].*");
 do
+	echo "### Installing $package ###"
 	sudo apt install $package -y
 done
 
