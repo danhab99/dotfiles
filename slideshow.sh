bf98f9cfb10aa5517e7b8ea84dc53eff089a59d3
Dan Habot <dan.habot@gmail.com>
2019-03-16 22:31:32 -0400

    Sat Mar 16 22:31:32 EDT 2019

--- slideshow.sh
+++ slideshow.sh
@@ -3,11 +3,10 @@
 mkdir -p $HOME/Pictures/wallpaper/.cache
 
 while true; do
-	ls *.png | shuf > .cache/list
-	for f in $(cat list); do
+	for f in ./*.png; do
 		cp -f "$f" .cache/current_wallpaper.png
-		feh --bg-scale .cache/current_wallpaper.png
 		convert .cache/current_wallpaper.png -blur 0x8 -resize 1920x1080! .cache/blurred_wallpaper.png
+		feh --bg-scale .cache/current_wallpaper.png
 		sleep 10m
 	done
 done
