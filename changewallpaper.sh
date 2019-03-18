fdb4b3de470ec0a8a3ce0101f8fff351cc9f2a65
Dan Habot <dan.habot@gmail.com>
2019-03-18 13:58:17 -0400

    Mon Mar 18 13:58:17 EDT 2019

--- changewallpaper.sh
+++ changewallpaper.sh
@@ -6,10 +6,19 @@ if [ ! -f $HOME/Pictures/wallpaper/.cache/.LOCK ]; then
 	touch $HOME/Pictures/wallpaper/.cache/.LOCK
 else
 	echo "Lock file exists"
+	echo ""
+	echo "Please run sudo rm $HOME/Pictures/wallpaper/.cache/.LOCK to fix"
 	exit 1
 fi
 
-cp -f "$1" .cache/current_wallpaper.png
+if [[ $1 =~ http.* ]]; then
+	echo "Downloading..."
+	curl -o- $1 > .cache/current_wallpaper.png
+else
+	echo "Copying..."
+	cp -f "$1" .cache/current_wallpaper.png
+fi
+
 convert .cache/current_wallpaper.png -blur 0x8 -resize 1920x1080! .cache/blurred_wallpaper.png
 feh --bg-scale .cache/current_wallpaper.png
 
