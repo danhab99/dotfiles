59d244c63a7ec43ca6085ba8360ce38b4bbfe2c7
Dan Habot <dan.habot@gmail.com>
2019-03-23 11:42:56 -0400

    Sat Mar 23 11:42:56 EDT 2019

--- slideshow.sh
+++ slideshow.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-mkdir -p $HOME/Pictures/wallpaper/.cache
+here=$HOME/Pictures/wallpaper
+
+mkdir -p $here/.cache
 
 while true; do
-	for f in $HOME/Pictures/wallpaper/*.png; do
-		source ./changewallpaper.sh "$f"
-		#cp -f "$f" .cache/current_wallpaper.png
-		#convert .cache/current_wallpaper.png -blur 0x8 -resize 1920x1080! .cache/blurred_wallpaper.png
-		#feh --bg-scale .cache/current_wallpaper.png
-		sleep 5m
+	for f in $here/*.png; do
+		source $here/changewallpaper.sh "$f"
+		notify-send "Changed wallpaper to $f"
+		sleep 1m
 	done
 done
