29f849f2ab5ed4f9472a757c507873dcf5b38037
Dan Habot <dan.habot@gmail.com>
2019-08-18 13:08:03 -0400

    Refactored package sync

--- install.sh
+++ install.sh
@@ -28,38 +28,5 @@ ln -s $HOME/Documents/dotfiles/update.sh $HOME/.local/bin/dotfiles-update
 ln -s $HOME/Documents/dotfiles/install.sh $HOME/.local/bin/dotfiles-install
 ln -s $HOME/Pictures/wallpaper/changewallpaper.sh $HOME/.local/bin/changewallpaper
 
-read -p "Do you want to install the packages [N/y]" yn
-
-if [[ ! yn =~ ^[Nn]$ ]];  then
-	echo "Skipping"
-else
-	echo "Adding PPAs"
-	apt-cache policy | grep http | awk '{print $2 $3}' | sort -u > /tmp/ppa.list
-
-	for ppa in $(diff -u /tmp/ppa.list ppa.list | grep -Po "(?<=\+)http.*");
-	do
-		echo "### Adding $ppa ###"
-		sudo add-apt-repository $ppa -y
-	done
-
-	sudo apt update
-
-	echo "Installing packages"
-	apt-mark showmanual > /tmp/packages.list
-
-	for package in $(diff -u /tmp/packages.list packages.list | grep -Po "(?<=\+)[a-zA-Z].*");
-	do
-		echo "### Installing $package ###"
-		sudo apt install $package -y
-	done
-
-	ls `npm root -g` > /tmp/npm.list
-
-	for package in $(diff -u /tmp/npm.list npm.list | grep -Po "(?<=\+)[a-zA-Z].*");do
-		echo "### Installing npm $package ###"
-		sudo npm install -g $package
-	done
-fi
-
 i3-msg restart
 cd $lwd
