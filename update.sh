4bc7d7c9b42f82dc08ecb30e7cf1c7c5ae5b7c78
Dan Habot <dan.habot@gmail.com>
2021-03-01 13:53:01 -0500

    Mon 01 Mar 2021 01:53:01 PM EST

--- update.sh
+++ update.sh
@@ -12,8 +12,11 @@ cp $HOME/.X* .
 echo "Copying packages"
 apt-mark showmanual > packages.list
 
-echo "Copying ppas"
-apt-cache policy | grep http | awk '{print $2 $3}' | sort -u > ppa.list
+#echo "Copying ppas"
+#apt-cache policy | grep http | awk '{print $2 $3}' | sort -u > ppa.list
+
+echo "Copying source list"
+cp /etc/apt/sources.list .
 
 echo "Copying brew"
 brew list > brew.list
