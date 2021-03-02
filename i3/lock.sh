951084a1545d22ad3164bace12171a4dd4b9d6bd
Dan Habot <dan.habot@gmail.com>
2021-03-02 15:58:36 -0500

    Tue 02 Mar 2021 03:58:36 PM EST

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 notify-send "DUNST_COMMAND_PAUSE"
+amixer -D pulse sset Master 0%
+
 i3lock -t -i "$(grep -P -o '(?<=file=).*' ~/.config/nitrogen/bg-saved.cfg)" -n
+
 notify-send "DUNST_COMMAND_RESUME"
+amixer -D pulse sset Master 100%
