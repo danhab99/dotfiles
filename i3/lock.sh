ddada06289b54cf1821348d56b09a42be1e21a85
Dan Habot <dan.habot@gmail.com>
2021-03-22 21:56:15 -0400

    Mon 22 Mar 2021 09:56:15 PM EDT

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 notify-send "DUNST_COMMAND_PAUSE"
-amixer -D pulse sset Master 0%
-playerctl play-pause
+MEDIA_STATUS=$(playerctl status)
+playerctl pause
 adb shell input keyevent 26 &
 
 i3lock -t -i "$(grep -P -o '(?<=file=).*' ~/.config/nitrogen/bg-saved.cfg)" -n
 
 notify-send "DUNST_COMMAND_RESUME"
-amixer -D pulse sset Master 100%
-playerctl play-pause
+
+if [ $MEDIA_STATUS = "Playing" ]; then
+  playerctl play
+fi
