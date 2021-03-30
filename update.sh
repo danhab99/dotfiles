d42cd0304b31ad69cdfa38ff15ee7d72201c0e7d
Dan Habot <dan.habot@gmail.com>
2021-03-29 21:24:18 -0400

    cleaning up old files

--- update.sh
+++ update.sh
@@ -9,36 +9,9 @@ cp $HOME/.bash_paths .bash_paths
 cp $HOME/.bashrc .bashrc
 cp $HOME/.X* .
 
-echo "Copying packages"
-apt-mark showmanual > packages.list
-
-#echo "Copying ppas"
-#apt-cache policy | grep http | awk '{print $2 $3}' | sort -u > ppa.list
-
-echo "Copying source list"
-cp /etc/apt/sources.list .
-
-echo "Copying brew"
-brew list --formula > brew.list
-
-echo "Copying npm globals"
-ls `npm root -g` > npm.list
-
 echo "Copying zsh"
 cp $HOME/.zshrc ./.zshrc
 cp -r $HOME/.oh-my-zsh/* ./oh-my-zsh/
-
-echo "Copying bashrc"
-
-#echo "Copying variety"
-#cp -r $HOME/.config/variety ./
-
-echo "Copying gtk*"
-cp -r $HOME/.config/gtk* ./
-
-echo "Copying scripts"
-cp $HOME/Pictures/wallpaper/*.sh ./
-cp $HOME/Documents/clipboard/*.sh ./clipboard/
 
 echo "Copying rofi"
 cp -r $HOME/.local/share/rofi/themes/* ./rofi/
