d951622ef914f4e62753f6bbf0ab48220686243e
Dan Habot <dan.habot@gmail.com>
2021-04-17 21:30:46 -0400

    Sat Apr 17 09:30:46 PM EDT 2021

--- i3/lock.sh
+++ i3/lock.sh
@@ -2,7 +2,7 @@
 notify-send "DUNST_COMMAND_PAUSE"
 MEDIA_STATUS=$(playerctl status)
 playerctl pause
-adb shell input keyevent 26 &
+# adb shell input keyevent 26 &
 
 i3lock -t -i "$(grep -P -o '(?<=file=).*' ~/.config/nitrogen/bg-saved.cfg)" -n
 
