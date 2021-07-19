fb80caec2648bbbbe1ee4227d62595b385b0a75c
Dan Habot <dan.habot@gmail.com>
2021-07-19 11:18:17 +0300

    better install procedure

--- install-packages.sh
+++ install-packages.sh
@@ -1,10 +1,9 @@
 echo "### Installing PACMAN packages ###"
 
-pacman -Syyy
+sudo pacman -Syyy
 
-cat pacman.list | while read line
-do
-  echo "### Installing $line ###"
-  L=$(echo $line | grep -o "^\S*")
-  yes | sudo pacman -S "$L"
-done
+# https://wiki.archlinux.org/title/Pacman/Tips_and_tricks#Install_packages_from_a_list
+sudo pacman -S --needed - < pacman.list
+sudo pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort pacman.list))
+sudo pacman -Rsu $(comm -23 <(pacman -Qq | sort) <(sort pacman.list))
+
