a670fcdb5a7ec05fbbd819c8639bc85f336ee932
Dan Habot <dan.habot@gmail.com>
2023-07-11 01:37:50 -0400

    Tue Jul 11 01:35:14 AM EDT 2023

--- .oh-my-zsh/plugins/nvm/nvm.plugin.zsh
+++ .oh-my-zsh/plugins/nvm/nvm.plugin.zsh
@@ -16,66 +16,56 @@ fi
 # Note: nvm is a function so we need to use `which`
 which nvm &>/dev/null && return
 
-# TODO: 2022-11-11: Remove soft-deprecate options
-if (( ${+NVM_LAZY} + ${+NVM_LAZY_CMD} + ${+NVM_AUTOLOAD} )); then
-  # Get list of NVM_* variable settings defined
-  local -a used_vars
-  used_vars=(${(o)parameters[(I)NVM_(AUTOLOAD|LAZY|LAZY_CMD)]})
-  # Nicely print the list in the style `var1, var2 and var3`
-  echo "${fg[yellow]}[nvm plugin] Variable-style settings are deprecated. Instead of ${(j:, :)used_vars[1,-2]}${used_vars[-2]+ and }${used_vars[-1]}, use:\n"
-  if (( $+NVM_AUTOLOAD )); then
-    echo "  zstyle ':omz:plugins:nvm' autoload true"
-    zstyle ':omz:plugins:nvm' autoload yes
-  fi
-  if (( $+NVM_LAZY )); then
-    echo "  zstyle ':omz:plugins:nvm' lazy true"
-    zstyle ':omz:plugins:nvm' lazy yes
-  fi
-  if (( $+NVM_LAZY_CMD )); then
-    echo "  zstyle ':omz:plugins:nvm' lazy-cmd $NVM_LAZY_CMD"
-    zstyle ':omz:plugins:nvm' lazy-cmd $NVM_LAZY_CMD
-  fi
-  echo "$reset_color"
-  unset used_vars NVM_AUTOLOAD NVM_LAZY NVM_LAZY_CMD
+if [[ -z "$NVM_DIR" ]]; then 
+  echo "[oh-my-zsh] nvm installation cannot be found"
+  echo "[oh-my-zsh] set NVM_DIR to your installation"
+  return
+fi
+if [[ ! -f "$NVM_DIR/nvm.sh" ]]; then 
+  echo "[oh-my-zsh] nvm.sh does not exist in $NVM_DIR"
+  return
 fi
 
-if zstyle -t ':omz:plugins:nvm' lazy; then
+if zstyle -t ':omz:plugins:nvm' lazy && \
+  ! zstyle -t ':omz:plugins:nvm' autoload; then
   # Call nvm when first using nvm, node, npm, pnpm, yarn or other commands in lazy-cmd
   zstyle -a ':omz:plugins:nvm' lazy-cmd nvm_lazy_cmd
   eval "
-    function nvm node npm pnpm yarn $nvm_lazy_cmd {
-      unfunction nvm node npm pnpm yarn $nvm_lazy_cmd
+    function nvm node npm npx pnpm yarn $nvm_lazy_cmd {
+      unfunction nvm node npm npx pnpm yarn $nvm_lazy_cmd
       # Load nvm if it exists in \$NVM_DIR
       [[ -f \"\$NVM_DIR/nvm.sh\" ]] && source \"\$NVM_DIR/nvm.sh\"
       \"\$0\" \"\$@\"
     }
   "
   unset nvm_lazy_cmd
-elif [[ -f "$NVM_DIR/nvm.sh" ]]; then
-  # Load nvm if it exists in $NVM_DIR
-  source "$NVM_DIR/nvm.sh"
 else
-  return
+  source "$NVM_DIR/nvm.sh"
 fi
 
 # Autoload nvm when finding a .nvmrc file in the current directory
 # Adapted from: https://github.com/nvm-sh/nvm#zsh
 if zstyle -t ':omz:plugins:nvm' autoload; then
-  load-nvmrc() {
+  function load-nvmrc {
     local node_version="$(nvm version)"
     local nvmrc_path="$(nvm_find_nvmrc)"
+    local nvm_silent=""
+    zstyle -t ':omz:plugins:nvm' silent-autoload && nvm_silent="--silent"
 
     if [[ -n "$nvmrc_path" ]]; then
-      local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+      local nvmrc_node_version=$(nvm version $(cat "$nvmrc_path" | tr -dc '[:print:]'))
 
       if [[ "$nvmrc_node_version" = "N/A" ]]; then
         nvm install
       elif [[ "$nvmrc_node_version" != "$node_version" ]]; then
-        nvm use
+        nvm use $nvm_silent
       fi
     elif [[ "$node_version" != "$(nvm version default)" ]]; then
-      echo "Reverting to nvm default version"
-      nvm use default
+      if [[ -z $nvm_silent ]]; then
+        echo "Reverting to nvm default version"
+      fi
+
+      nvm use default $nvm_silent
     fi
   }
 
