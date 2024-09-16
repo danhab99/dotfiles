8c68a92d1ebb06c695f5a723c0092e2552027ee2
Dan Habot <dan.habot@gmail.com>
2024-09-16 16:39:25 -0400

    gotta track hardware-configuration

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -24,6 +24,13 @@
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
+  fileSystems."/home" = {
+    device = "/dev/sda";
+    fsType = "ext4";
+    options = [ "noatime" ]; # Optional: Add mount options as needed
+  };
+
+
   swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
