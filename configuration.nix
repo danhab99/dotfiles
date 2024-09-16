8353a0ecdcfb92403375a79b7d557f0557f8ad02
Dan Habot <dan.habot@gmail.com>
2024-09-15 21:57:16 -0400

    distro hopped to nix

---
+++ configuration.nix
@@ -0,0 +1,195 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver = {
+    enable = true;
+    desktopManager.xterm.enable = false;
+    windowManager.i3 = {
+      enable = true;
+      package = pkgs.i3-rounded;
+    };
+  };
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  services.displayManager.defaultSession = "none+i3";
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.dan = {
+    isNormalUser = true;
+    description = "dan";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    packages = with pkgs; [
+      kate
+    #  thunderbird
+    ];
+  };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    git
+    curl
+    i3-rounded
+    i3status
+    autorandr
+    arandr
+    rxvt-unicode
+    rofi
+    brave
+    gnumake
+    clipit
+    zsh
+    fira-code
+    fira-code-nerdfont
+    i3blocks
+    zoxide
+    fzf
+    nodejs_22
+    nitrogen
+    ripgrep
+    docker
+    rsync
+    rclone
+    ranger
+    htop
+    iotop
+    picom
+    gnupg
+  ];
+
+  fonts.packages = with pkgs; [
+    fira-code
+    fira-code-symbols
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+    pinentryFlavor = "curses";
+  };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  virtualisation.docker = {
+    enable = true;
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+    };
+  };
+
+  # fileSystems."/" = {
+  #   device = "/dev/nvme0n1p2"; # Adjust as needed for your root partition
+  #   fsType = "ext4"; # Or your root partition's file system type
+  # };
+
+  fileSystems."/home" = {
+    device = "/dev/sda";
+    fsType = "ext4";
+    options = [ "noatime" ]; # Optional: Add mount options as needed
+  };
+
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable; services.xserver.videoDrivers = [ "nvidia" ];
+}
---
+++ home.nix
@@ -0,0 +1,153 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "dan";
+  home.homeDirectory = "/home/dan";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    nerdfonts
+    (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
+    powerline-fonts
+    fira-code
+    fontconfig
+  ]; 
+
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      monospace = [ "fira-code" ];
+      sansSerif = [ "fira-code" ];
+      serif = [ "fira-code" ];
+    };
+  };
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    ".config/coc/extensions/package.json" = {
+      source = ./config/coc/package.json;
+    };
+
+    ".config/g600" = {
+      source = ./g600;
+      recursive = true;
+    };
+
+    ".config/dunst" = {
+      source = ./config/dunst;
+    };
+
+    ".config/i3" = {
+      source = ./config/i3;
+      recursive = true;
+    };
+
+    ".config/rofi" = {
+      source = ./config/rofi;
+      recursive = true;
+    };
+
+    # ".oh-my-zsh" = {
+    #   source = ./zsh/oh-my-zsh;
+    #   recursive = true;
+    # };
+
+    ".zshrc" = {
+      source = ./zsh/zshrc;
+    };
+
+    # ".screenlayout" = {
+    #   source = ./config/screenlayout;
+    #   recursive = true;
+    # };
+
+    # ".urxvt/ext" = {
+    #   source = ./urxvt/ext;
+    # };
+
+    ".vim/coc-settings.json" = {
+      source = ./vim/coc-settings.json;
+    };
+
+    ".vim/desert256.vim" = {
+      source = ./vim/desert256.vim;
+    };
+
+    ".vimrc" = {
+      source = ./vim/vimrc;
+    };
+
+    ".bash_aliases" = {
+      source = ./bash/bash_aliases;
+    };
+
+    ".bash_paths" = {
+      source = ./bash/bash_paths;
+    };
+
+    ".bash_profile" = {
+      source = ./bash/bash_profile;
+    };
+
+    ".bashrc" = {
+      source = ./bash/bashrc;
+    };
+
+    ".gitignore" = {
+      source = ./git/gitignore;
+    };
+
+    ".gitconfig" = {
+      source = ./git/gitconfig;
+    };
+
+    # ".xbindkeys" = {
+    #   source = ./X/xbindkeys;
+    # };
+
+    ".Xdefaults" = {
+      source = ./X/Xdefaults;
+    };
+
+    ".Xresources" = {
+      source = ./X/Xresources;
+    };
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/dan/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    EDITOR = "vim";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+}
