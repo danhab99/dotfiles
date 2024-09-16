70bee1dfb0cebf619be9ac5ee2efb8bcf9e45bc3
Dan Habot <dan.habot@gmail.com>
2024-09-16 16:39:33 -0400

    changes

--- flake.nix
+++ flake.nix
@@ -1,22 +1,42 @@
 {
-  description = "";
+  description = "Your new nix config";
 
   inputs = {
-      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+
+    # Home manager
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }:
-  let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-  in
-  {
-    workstation =
-      pkgs.mkShell
-        {
-          buildInputs = with pkgs; [
-            gnupg
-          ];
-        };
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+  in {
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
+    nixosConfigurations = {
+      workstation = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        # > Our main nixos configuration file <
+        modules = [./configuration.nix ./hardware-configuration.nix];
+      };
+    };
+
+    # Standalone home-manager configuration entrypoint
+    # Available through 'home-manager --flake .#your-username@your-hostname'
+    homeConfigurations = {
+      "dan@workstation" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = {inherit inputs outputs;};
+        # > Our main home-manager configuration file <
+        modules = [./home.nix];
+      };
+    };
   };
 }
