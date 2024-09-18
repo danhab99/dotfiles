b69239c1d06c69b8aef8b752d2110d6f54942b8f
Dan Habot <dan.habot@gmail.com>
2024-09-18 09:35:48 -0400

    fixed vim clipboard

--- configuration.nix
+++ configuration.nix
@@ -106,7 +106,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim
+    vim-full
     wget
     git
     curl
