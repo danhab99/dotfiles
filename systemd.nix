c5307784eeaf21e795f45073a27c9bbf452bf0d1
Dan Habot <dan.habot@gmail.com>
2024-11-02 21:49:55 -0400

    changes

--- systemd.nix
+++ systemd.nix
@@ -1,24 +1,15 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   services."backup" = {
     script = ''
       set -eu
 
-      current_date=$(date +%s)
       mkdir -p /bucket/backup/$(date +%Y-%m-%d)
 
-      tar -czvf /bucket/backup/$(date +%Y-%m-%d)/home.tar.gz /home &
-      tar -czvf /bucket/backup/$(date +%Y-%m-%d)/nixstore.tar.gz /nix/store &
+      ${pkgs.toybox}/bin/tar -czvf /bucket/backup/$(date +%Y-%m-%d)/home.tar.gz /home &
+      ${pkgs.toybox}/bin/tar -czvf /bucket/backup/$(date +%Y-%m-%d)/nixstore.tar.gz /nix/store &
       wait
-
-      for folder in /bucket/backup/*; do
-        folder_date=$(date -r "$folder" +%s)
-        days_old=$(( (current_date - folder_date) / (60*60*24) ))
-        if [ "$days_old" -gt 7 ]; then
-          rm -rf "$folder"
-        fi
-      done
     '';
     serviceConfig = {
       Type = "oneshot";
