bfe650dc93900abe8c865cec0729861e6e5999ab
Dan Habot <dan.habot@gmail.com>
2024-11-17 23:13:58 -0500

    changes

--- home.nix
+++ home.nix
@@ -48,6 +48,15 @@
       recursive = true;
     };
 
+    # ".config/rofi" = {
+    #   source = builtins.fetchGit {
+    #     shallow = true;
+    #     url = "https://github.com/adi1090x/rofi.git";
+    #     rev = "86e6875d9e89ea3cf95c450cef6497d52afceefe";
+    #   };
+    #   recursive = true;
+    # };
+
     ".config/rofi" = {
       source = ./rofi;
       recursive = true;
