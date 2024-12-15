10102c6d77bc9c348a45b9055aaa1b997bd67dd4
Dan Habot <dan.habot@gmail.com>
2024-12-15 00:04:18 -0500

    extracting common configs

---
+++ machine/files.nix
@@ -0,0 +1,49 @@
+{ }:
+let src = name: ../config/${name};
+in {
+  ".config/g600" = {
+    source = src "g600";
+    recursive = true;
+  };
+
+  ".config/i3blocks.conf" = {
+    source = src "i3blocks/i3blocks.conf";
+  };
+
+  ".config/picom.conf" = {
+    source = src "picom/picom.conf";
+  };
+
+  ".config/rofi" = {
+    source = builtins.fetchGit {
+      shallow = true;
+      url = "https://github.com/adi1090x/rofi.git";
+      rev = "86e6875d9e89ea3cf95c450cef6497d52afceefe";
+    };
+    recursive = true;
+  };
+
+  ".vim/desert256.vim" = { source = src "vim/desert256.vim"; };
+
+  ".vimrc" = { source = src "./vim/vimrc"; };
+
+  ".Xdefaults" = { source = src "./X/Xdefaults"; };
+
+  ".Xresources" = { source = src "./X/Xresources"; };
+
+  ".urxvt/ext" = {
+    source = src "urxvt/ext";
+    recursive = true;
+  };
+
+  ".config/ev-cmd.toml" = { source = src "ev-cmd/ev-cmd.toml"; };
+
+  ".config/i3blocks-contrib" = {
+    source = builtins.fetchGit {
+      url = "https://github.com/vivien/i3blocks-contrib.git";
+      rev = "9d66d81da8d521941a349da26457f4965fd6fcbd";
+      shallow = true;
+    };
+    recursive = true;
+  };
+}
---
+++ machine/home.nix
@@ -0,0 +1,42 @@
+{ pkgs, ... }:
+
+{
+  imports = [ ../programs/zsh.home.nix ../programs/git.home.nix ];
+
+  home.username = "dan";
+  home.homeDirectory = "/home/dan";
+
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      monospace = [ "fira-code" ];
+      sansSerif = [ "fira-code" ];
+      serif = [ "fira-code" ];
+    };
+  };
+
+  home.file = import ./files.nix { };
+
+  home.sessionVariables = {
+    EDITOR = pkgs.vim-full + "/bin/vim";
+    BROWSER = pkgs.brave + "/bin/brave";
+    VI_MODE_SET_CURSOR = "true";
+    VI_MODE_RESET_PROMPT_ON_MODE_CHANGE = "true";
+  };
+
+  programs.home-manager.enable = true;
+
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+}
