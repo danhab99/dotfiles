3f37cabdb900523efce5e4f15869a591cedbb2c6
Dan Habot <dan.habot@gmail.com>
2024-12-17 00:17:16 -0500

    fix settings

--- programs/gnupg.nix
+++ programs/gnupg.nix
@@ -1,13 +1,13 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
-    # settings = {
-    #   default-cache-ttle = 3 * 8.64e+7;
-    #   max-cache-ttl = 3 * 8.64e+7;
-    # };
-    # pinentryFlavor = "curses";
+    settings = {
+      default-cache-ttle = 3 * 8.64e+7;
+      max-cache-ttl = 3 * 8.64e+7;
+    };
+    pinentryPackage = pkgs.pinentry-curses;
   };
 }
