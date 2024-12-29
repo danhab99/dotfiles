0462a5a4fb59776ded01b36072111b4f927486ed
Dan Habot <dan.habot@gmail.com>
2024-12-29 18:23:27 -0500

    moved packages

--- program/java.nix
+++ program/java.nix
@@ -1,6 +1,13 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  environment.systemPackages = with pkgs; [
+    java-language-server
+    jdk21_headless
+    jdk8_headless
+    openjdk
+  ];
+
   programs.java = {
     enable = false;
     # package = pkgs.oraclejre8;
