aec2d2a35a2461f386523cdb6152fc10d2c2c22e
Dan Habot <dan.habot@gmail.com>
2025-03-05 10:50:15 -0500

    changes

--- machine/configuration.nix
+++ machine/configuration.nix
@@ -33,12 +33,57 @@
     };
   };
 
-  environment.systemPackages = with pkgs; [
-    swt
-    nix-ld
+  environment.systemPackages = let
+    customBusybox = pkgs.busybox.overrideAttrs (oldAttrs: rec {
+      postInstall = ''
+        ${oldAttrs.postInstall or ""}
+        # Remove the reboot symlink if it exists
+        rm -f $out/bin/reboot
+        rm -f $out/bin/host*
+      '';
+    });
+  in with pkgs; [
+    alsa-utils
+    arandr
+    astyle
+    autorandr
+    base16-schemes
+    customBusybox
+    file
+    fontforge
+    git
+    gnumake
     gnupg
-    pinentry-curses
-    pass
+    gnutar
+    gparted
+    gzip
+    kdePackages.kcalc
+    killall
+    linuxKernel.packages.linux_zen.nvidia_x11
+    lm_sensors
+    nemo
+    nil
+    nix-ld
+    nodejs_22
+    nvtopPackages.full
+    pamixer
+    pavucontrol
+    pulseaudioFull
+    rofi
+    rxvt-unicode
+    sutils
+    swt
+    sysstat
+    unzip
+    usbutils
+    wget
+    xclip
+    xdotool
+    xorg.xev
+    xpad
+    xsel
+    yai
+    zip
   ];
 
   environment.variables = with pkgs; {
--- machine/workstation/home.nix
+++ machine/workstation/home.nix
@@ -1,9 +1,29 @@
-{ ... }:
+{ config, option, ... }:
 
 {
-  imports = [ ./user.nix ];
+  imports = [ ../../modules/default.nix ];
 
-  home.file = {
+  config.modules = {
+    appimage.enable = true;
+    git.enable = true;
+    gnupg.enable = true;
+    i3 = {
+      enable = true;
+      configFile = ./i3/config;
+    };
+    ollama = {
+      enable = false;
+      repoDir = "/bucket/ollama";
+    };
+    picom.enable = true;
+    urxvt.enable = true;
+    vim.enable = true;
+    xorg.enable = true;
+    zoxide.enable = true;
+    zsh.enable = true;
+  };
+
+  config.home.file = {
     ".config/g600" = {
       source = ./g600;
       recursive = true;
