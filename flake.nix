5f4580ea47479ddf88fd38b4c36e5124a358b54d
Dan Habot <dan.habot@gmail.com>
2025-03-06 23:05:02 -0500

    update

--- flake.nix
+++ flake.nix
@@ -1,43 +1,79 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    nur.url = "github:nix-community/NUR";
-    nixos-cli.url = "github:water-sucks/nixos";
-    stylix.url = "github:danth/stylix";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixos-cli = {
+      url = "github:water-sucks/nixos";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    stylix = {
+      url = "github:danth/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, home-manager, nixos-cli, nur, stylix, ... }@inputs:
     let
       inherit (self) outputs;
-
-      mkNix = system: hostname:
+      # Supported systems for your flake
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      # Helper function to generate a set of attributes for each system
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+      # Helper function to create default pkgs instance for each system
+      pkgsFor = system: import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      
+      mkNix = hostname:
         nixpkgs.lib.nixosSystem {
           specialArgs = { inherit inputs outputs; };
-          system = system;
           modules = [
             stylix.nixosModules.stylix
             nixos-cli.nixosModules.nixos-cli
-            { networking.hostName = hostname; }
-
-            ./machine/${hostname}/hardware-configuration.nix
-            ./machine/${hostname}/configuration.nix
             ./machine/configuration.nix
-
+            ./machine/${hostname}/configuration.nix
+            ./machine/${hostname}/hardware-configuration.nix
             home-manager.nixosModules.home-manager
             {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                extraSpecialArgs = { inherit inputs; };
-                users.dan = ./machine/${hostname}/home.nix;
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.dan = {
+                imports = [ ./machine/home.nix ./machine/${hostname}/home.nix ];
               };
             }
           ];
         };
     in {
       nixosConfigurations = {
-        workstation = mkNix "x86_64-linux" "workstation";
+        workstation = mkNix "workstation";
+      };
+      
+      # Standalone home-manager configuration
+      homeConfigurations = {
+        "dan" = forAllSystems (system:
+          home-manager.lib.homeManagerConfiguration {
+            pkgs = pkgsFor system;
+            extraSpecialArgs = {
+              inherit inputs outputs;
+            };
+            modules = [
+              ./machine/home.nix
+              ./machine/workstation/home.nix  # You might want to make this more generic
+              # stylix.homeManagerModules.stylix
+            ];
+          });
       };
     };
 }
