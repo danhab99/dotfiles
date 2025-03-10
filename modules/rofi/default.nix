9929afa10f9bf65cf416ea1e16ba59eade25b209
Dan Habot <dan.habot@gmail.com>
2025-03-10 00:31:39 -0400

    restored derivation

--- modules/rofi/default.nix
+++ modules/rofi/default.nix
@@ -10,13 +10,25 @@ in {
     home.packages = with pkgs; [ rofi ];
 
     home.file = {
-      ".config/rofi" = {
-        source = builtins.fetchGit {
-          shallow = true;
-          url = "https://github.com/adi1090x/rofi.git";
-          rev = "86e6875d9e89ea3cf95c450cef6497d52afceefe";
-        };
+      ".config/rofi" = let rev = "86e6875d9e89ea3cf95c450cef6497d52afceefe";
+      in {
+        source = stdenv.mkDerivation {
+          name = "adi1090x/rofi";
+          version = rev;
 
+          src = builtins.fetchGit {
+            shallow = true;
+            url = "https://github.com/adi1090x/rofi.git";
+            inherit rev;
+          };
+
+          installPhase = ''
+            cd $src
+            chmod +x setup.sh
+            ./setup.sh
+            cp $src $out
+          '';
+        };
         recursive = true;
       };
     };
