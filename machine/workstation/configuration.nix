3b34f45b2ea67d88a0457512e54b66a544c8c8ca
Dan Habot <dan.habot@gmail.com>
2025-03-15 12:37:56 -0400

    work

--- machine/workstation/configuration.nix
+++ machine/workstation/configuration.nix
@@ -3,14 +3,17 @@
 {
   imports = [ ../../parts/default.nix ];
 
-  config.modules = {
+  config.part = {
     appimage.enable = true;
     docker.enable = true;
     font.enable = true;
+    gnupg.enable = true;
     i18n.enable = true;
+    i3.enable = true;
     nix.enable = true;
     packages.enable = true;
     pipewire.enable = true;
+    printing.enable = true;
     ratbag.enable = true;
     sddm.enable = true;
     secrets.enable = true;
@@ -18,6 +21,7 @@
     steam.enable = true;
     timezone.enable = true;
     xserver.enable = true;
+    zsh.enable = true;
   };
 
   config = {
@@ -26,6 +30,11 @@
       description = "dan";
       extraGroups = [ "networkmanager" "wheel" "docker" "input" "dialout" ];
       shell = pkgs.zsh;
+    };
+
+    networking = { 
+      networkmanager.enable = true;
+      hostName = "workstation";
     };
 
     environment.localBinInPath = true;
@@ -65,5 +74,71 @@
           Option "TripleBuffer" "True"
       EndSection
     '';
+
+    systemd = {
+      tmpfiles.rules = [
+        "L+ /home/dan/Videos - - - - /bucket/Videos"
+        "L+ /home/dan/Pictures - - - - /bucket/Pictures"
+      ];
+
+      services."backup" = {
+        script = ''
+          set -eu
+
+          function cleanup_old_backups() {
+            ${pkgs.findutils}/bin/find /bucket/backup -type f -name "*.tar.gz" -mtime +7 -delete
+          }
+
+          function backup() {
+            ${pkgs.gnutar}/bin/tar -cz \
+            --exclude-caches \
+            --exclude="**/node_modules" \
+            --exclude="**/*[Cc]ache*" \
+            --exclude="/home/dan/Videos" \
+            --exclude="/home/dan/Pictures" \
+            --seek \
+            -f /bucket/backup/$1.$(date +%Y-%m-%d).tar.gz $2
+          }
+
+          cleanup_old_backups
+          backup home /home/dan &
+
+          wait
+        '';
+        serviceConfig = {
+          Type = "oneshot";
+          User = "root";
+        };
+
+        path = with pkgs; [ gnutar gzip findutils ];
+      };
+
+      timers."backup" = {
+        wantedBy = [ "timers.target" ];
+        timerConfig = {
+          OnCalendar = "*-*-* 04:00:00";
+          Persistent = true;
+        };
+      };
+
+      services."download-music" = {
+        script = "/home/dan/Music/download.sh";
+
+        serviceConfig = {
+          Type = "oneshot";
+          User = "root";
+        };
+
+        path = with pkgs; [ gnutar gzip findutils ];
+      };
+
+      timers."download-music" = {
+        wantedBy = [ "timers.target" ];
+        timerConfig = {
+          OnCalendar = "*-*-* 02:00:00";
+          Persistent = true;
+        };
+      };
+    };
   };
 }
--- modules/rofi/default.nix
+++ modules/rofi/default.nix
@@ -12,23 +12,28 @@ in {
     home.file = {
       ".config/rofi" = let rev = "86e6875d9e89ea3cf95c450cef6497d52afceefe";
       in {
-        source = stdenv.mkDerivation {
-          name = "adi1090x/rofi";
-          version = rev;
-
-          src = builtins.fetchGit {
-            shallow = true;
-            url = "https://github.com/adi1090x/rofi.git";
-            inherit rev;
-          };
-
-          installPhase = ''
-            cd $src
-            chmod +x setup.sh
-            ./setup.sh
-            cp $src $out
-          '';
+        source = builtins.fetchGit {
+          shallow = true;
+          url = "https://github.com/adi1090x/rofi.git";
+          inherit rev;
         };
+        # source = stdenv.mkDerivation {
+        #   name = "adi1090x/rofi";
+        #   version = rev;
+
+        #   src = builtins.fetchGit {
+        #     shallow = true;
+        #     url = "https://github.com/adi1090x/rofi.git";
+        #     inherit rev;
+        #   };
+
+        #   installPhase = ''
+        #     cd $src
+        #     chmod +x setup.sh
+        #     ./setup.sh
+        #     cp $src $out
+        #   '';
+        # };
         recursive = true;
       };
     };
