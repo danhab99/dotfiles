d115a8458026b6e929427490e30af4dda8e3da98
Dan Habot <dan.habot@gmail.com>
2025-03-20 23:08:01 -0400

    deleted users

--- machine/laptop/configuration.nix
+++ machine/laptop/configuration.nix
@@ -3,30 +3,37 @@ let
   mkMachine = import ../mkMachine.nix { inherit pkgs; };
 in
 {
-  imports = [ ../../parts/default.nix ];
+  imports = [ ../../modules/default.nix ];
 
   config = mkMachine { hostName = "laptop"; } {
-    part = {
+    module = {
       appimage.enable = true;
       docker.enable = true;
       font.enable = true;
+      fzf.enable = true;
+      git.enable = true;
       gnupg.enable = true;
       i18n.enable = true;
-      i3.enable = true;
+      i3 = { enable = true; configFile = ./i3/config; };
       nix.enable = true;
+      ollama = { enable = false; repoDir = "/bucket/ollama"; };
       packages.enable = true;
+      picom.enable = true;
       pipewire.enable = true;
-      printing.enable = false;
+      printing.enable = true;
       ratbag.enable = false;
+      rofi.enable = true;
       sddm.enable = true;
       secrets.enable = true;
       ssh.enable = true;
       steam.enable = false;
+      threedtools.enable = false;
       timezone.enable = true;
-      xserver = {
-        enable = true;
-        videoDriver = "modesetting";
-      };
+      urxvt.enable = true;
+      vim.enable = true;
+      xorg.enable = true;
+      xserver = { enable = true; videoDriver = "nvidia"; };
+      zoxide.enable = true;
       zsh.enable = true;
     };
   };
