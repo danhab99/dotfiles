9cc1bc0ede7369d7780902b35724ccd6d4486dc8
Dan Habot <dan.habot@gmail.com>
2025-03-21 20:03:22 -0400

    blanked this out

--- modules/rofi/default.nix
+++ modules/rofi/default.nix
@@ -1,36 +1,35 @@
 import ../module.nix {
   name = "rofi";
 
-  output = { pkgs, ... }: {
+  output = { pkgs, lib, ... }: {
     packages = with pkgs; [ rofi ];
 
-    homeManager.home.file = {
-      ".config/rofi" = let rev = "86e6875d9e89ea3cf95c450cef6497d52afceefe";
-      in {
-        source = builtins.fetchGit {
-          shallow = true;
-          url = "https://github.com/adi1090x/rofi.git";
-          inherit rev;
-        };
-        # source = stdenv.mkDerivation {
-        #   name = "adi1090x/rofi";
-        #   version = rev;
+    # homeManager.home.file = {
+    #   ".config/rofi" = let rev = "86e6875d9e89ea3cf95c450cef6497d52afceefe";
+    #   in {
+    #     # source = builtins.fetchGit {
+    #     #   shallow = true;
+    #     #   url = "https://github.com/adi1090x/rofi.git";
+    #     #   inherit rev;
+    #     # };
+    #     source = pkgs.stdenv.mkDerivation {
+    #       name = "adi1090x/rofi";
+    #       version = rev;
 
-        #   src = builtins.fetchGit {
-        #     shallow = true;
-        #     url = "https://github.com/adi1090x/rofi.git";
-        #     inherit rev;
-        #   };
+    #       src = builtins.fetchGit {
+    #         shallow = true;
+    #         url = "https://github.com/adi1090x/rofi.git";
+    #         inherit rev;
+    #       };
 
-        #   installPhase = ''
-        #     cd $src
-        #     chmod +x setup.sh
-        #     ./setup.sh
-        #     cp $src $out
-        #   '';
-        # };
-        recursive = true;
-      };
-    };
+    #       installPhase = ''
+    #         cd $src
+    #         cp -r $src $out
+    #         . $out/setup.sh
+    #       '';
+    #     };
+    #     recursive = true;
+    #   };
+    # };
   };
 }
