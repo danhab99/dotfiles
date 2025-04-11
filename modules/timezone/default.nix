83a8aa1711377df420812db6575186455c3c6d9e
Dan Habot <dan.habot@gmail.com>
2025-04-10 22:02:11 -0400

    change timezone

--- modules/timezone/default.nix
+++ modules/timezone/default.nix
@@ -1,15 +1,8 @@
 import ../module.nix {
   name = "timezone";
 
-  output = { pkgs, ... }: {
-    environment.systemPackages = with pkgs;
-      [
-        # ...
-      ];
-
-    # nixos.time.timeZone = "America/New_York";
-    nixos.time.timeZone = "Asia/Jerusalem";
-
-    homeManager = { };
+  output = { ... }: {
+    nixos.time.timeZone = "America/New_York";
+    # nixos.time.timeZone = "Asia/Jerusalem";
   };
 }
