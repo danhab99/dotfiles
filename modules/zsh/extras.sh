61a57898d651e31d1220f78338944d06a32d61ff
Dan Habot <dan.habot@gmail.com>
2025-04-19 14:26:24 -0400

    removed these

--- modules/zsh/extras.sh
+++ modules/zsh/extras.sh
@@ -44,55 +44,6 @@ function ns() {
     nix develop .#$1
 }
 
-function notes() {
-  notes_path=~/Documents/notes
-  mkdir -p "$notes_path"
-  note_dir="$notes_path/$(basename "$(dirname "$PWD")")/$(basename "$PWD")"
-  note_path="$note_dir/$(date +"%Y-%m-%d")"
-  templates_path="$notes_path/templates"
-
-  case $1 in
-    ls)
-      echo "Notes in $note_dir"
-      ls -lth "$note_dir"
-      ;;
-    prev)
-      for file in "$note_dir"/*; do
-        if [[ -f "$file" ]]; then
-          bat -r :4 "$file"
-          echo
-        fi
-      done
-      ;;
-    template)
-      if [[ -z "$2" ]]; then
-        echo "Available templates:"
-        ls -1 "$templates_path" 2>/dev/null || echo "No templates found."
-      else
-        template_file="$templates_path/$2.md"
-        if [[ -f "$template_file" ]]; then
-          mkdir -p "$note_dir"
-          cp "$template_file" "$note_path.md"
-          vim "$note_path.md"
-        else
-          echo "Template '$2' not found in $templates_path"
-        fi
-      fi
-      ;;
-    *)
-      mkdir -p "$note_dir"
-      if [ -n "$1" ]; then
-        vim "$note_path--$1.md"
-      else
-        vim "$note_path.md"
-      fi
-      ;;
-  esac
-
-  git -C $notes_path add $notes_path
-  git -C $notes_path commit -m "$(date)" $notes_path
-}
-
 if [ -e /tmp/workdir ]
 then
   DIR=$(cat /tmp/workdir)
@@ -107,6 +58,3 @@ then
   shell_name=$(cat /tmp/nixshell | head -n 2 | tail -n 1)
   nix develop $flake_path#$shell_name
 fi
-
-# Set up fzf key bindings and fuzzy completion
-source <(fzf --zsh)
