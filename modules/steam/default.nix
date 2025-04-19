4484f334d0590d2d3aaa6c8dcad00092d79fd765
Dan Habot <dan.habot@gmail.com>
2025-04-19 17:30:16 -0400

    fix

--- modules/steam/default.nix
+++ modules/steam/default.nix
@@ -2,18 +2,17 @@ import ../module.nix {
   name = "steam";
 
   output = { pkgs, ... }: {
-    environment.systemPackages = with pkgs; [ ];
+    nixos = {
+      programs.steam = {
+        enable = true;
+        remotePlay.openFirewall = true;
+        dedicatedServer.openFirewall = false;
+        localNetworkGameTransfers.openFirewall = false; 
 
-    nixos.programs.steam = {
-      enable = true;
-      remotePlay.openFirewall =
-        false; # Open ports in the firewall for Steam Remote Play
-      dedicatedServer.openFirewall =
-        false; # Open ports in the firewall for Source Dedicated Server
-      localNetworkGameTransfers.openFirewall =
-        false; # Open ports in the firewall for Steam Local Network Game Transfers
+        protontricks.enable = true;
+      };
+
+      hardware.steam-hardware.enable = true;
     };
-
-    homeManager = { };
   };
 }
