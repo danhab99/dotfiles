9463ecdf811a3c3193e6175b0f36cbbf4ca7a718
Dan Habot <dan.habot@gmail.com>
2025-04-19 14:02:48 -0400

    rewrote mkMachine

--- machine/mkMachine.sh
+++ machine/mkMachine.sh
@@ -4,41 +4,29 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-# Create the directory if it doesn't exist
+# Create the target directory if it doesn't exist
 mkdir -p "$1"
 
-# Generate the Nix template and write it to the file
+# Start the configuration.nix file
 cat <<EOF > "$1/configuration.nix"
-{ pkgs, ... }:
-let
-  mkMachine = import ../mkMachine.nix { inherit pkgs; };
-in
-{
-  imports = [ ../../parts/default.nix ];
+{ ... }:
 
-  config = mkMachine { hostName = "$1"; } {
-    part = {
-      appimage.enable = true;
-      docker.enable = true;
-      font.enable = true;
-      gnupg.enable = true;
-      i18n.enable = true;
-      i3.enable = true;
-      nix.enable = true;
-      packages.enable = true;
-      pipewire.enable = true;
-      printing.enable = true;
-      ratbag.enable = true;
-      sddm.enable = true;
-      secrets.enable = true;
-      ssh.enable = true;
-      steam.enable = true;
-      timezone.enable = true;
-      xserver.enable = true;
-      zsh.enable = true;
-    };
+{
+  imports = [ ../../modules/default.nix ];
+
+  config.module = {
+EOF
+
+# List all directories in ../modules and add them as <name>.enable = true;
+for dir in ../modules/*/; do
+  name=$(basename "$dir")
+  echo "    $name.enable = true;" >> "$1/configuration.nix"
+done
+
+# Close the config block
+cat <<EOF >> "$1/configuration.nix"
   };
 }
 EOF
 
-echo "Machine generated at $1/default.nix"
+echo "Machine configuration generated at $1/configuration.nix"
