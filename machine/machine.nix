f689fe84b8d6350cb593e1df9289e1a026c08e87
Dan Habot <dan.habot@gmail.com>
2025-04-20 00:09:54 -0400

    modularized users

--- machine/machine.nix
+++ machine/machine.nix
@@ -1,23 +1,7 @@
 { hostName }:
-{ pkgs, ... }: 
+{ ... }: 
 
 {
-  users.users.dan = {
-    isNormalUser = true;
-    description = "dan";
-    extraGroups = [ 
-      "networkmanager" 
-      "wheel" 
-      "docker" 
-      "input" 
-      "dialout" 
-      "tty" 
-      "video" 
-      "plugdev" 
-    ];
-    shell = pkgs.zsh;
-  };
-
   networking = {
     networkmanager.enable = true;
     inherit hostName;
@@ -30,35 +14,6 @@
   services.nixos-cli = { enable = true; };
 
   boot.tmp.cleanOnBoot = true;
-
-  home-manager.users.dan = {
-    home.username = "dan";
-    home.homeDirectory = "/home/dan";
-
-    fonts.fontconfig = {
-      enable = true;
-      defaultFonts = {
-        monospace = [ "fira-code" ];
-        sansSerif = [ "fira-code" ];
-        serif = [ "fira-code" ];
-      };
-    };
-
-    home.sessionVariables = with pkgs; {
-      EDITOR = vim-full + "/bin/vim";
-      BROWSER = firefox + "/bin/firefox";
-      GIT_PAGER = bat + "/bin/bat";
-    };
-
-    # This value determines the Home Manager release that your configuration is
-    # compatible with. This helps avoid breakage when a new Home Manager release
-    # introduces backwards incompatible changes.
-    #
-    # You should not change this value, even if you update Home Manager. If you do
-    # want to update the value, then make sure to first check the Home Manager
-    # release notes.
-    home.stateVersion = "24.05"; # Please read the comment before changing.
-  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
