8b98535857af6e836969fc5f6dbed8f82486a63a
Dan Habot <dan.habot@gmail.com>
2025-04-19 23:40:06 -0400

    rewrote modules to be per user

--- modules/module.nix
+++ modules/module.nix
@@ -3,17 +3,36 @@
 let
   cfg = config.module.${name};
   out = output { inherit lib config pkgs cfg; };
+
+  enabledNixos = if out ? nixos then out.nixos else { };
+  enabledPackages = if (out ? packages) then out.packages else [ ];
+  enabledHomemanager = if (out ? homeManager) then out.homeManager else { };
+
+  enabledHomemanagerForUses =
+    let
+      pairs = builtins.map
+        (user: {
+          name = user;
+          value = enabledHomemanager;
+        })
+        cfg.forUsers;
+    in
+    builtins.listToAttrs pairs;
 in
 {
   options.module.${name} = (options { inherit lib; }) // {
     enable = lib.mkEnableOption name;
+    forUsers = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ "dan" ];
+    };
   };
 
   config = lib.mkIf cfg.enable (
-    (if out ? nixos then out.nixos else { }) // 
+    enabledNixos //
     {
-      environment.systemPackages = if (out ? packages) then out.packages else [ ];
-      home-manager.users.dan.config = if (out ? homeManager) then out.homeManager else { };
+      environment.systemPackages = enabledPackages;
+      home-manager.users.dan.config = enabledHomemanager;
     }
   );
 }
