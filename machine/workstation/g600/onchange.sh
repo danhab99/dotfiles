38d462d31d0d85a8fce686bab27c167588493893
Dan Habot <dan.habot@gmail.com>
2025-04-30 21:02:03 -0400

    simplified change script

--- machine/workstation/g600/onchange.sh
+++ machine/workstation/g600/onchange.sh
@@ -1,13 +1,5 @@
 ratbagctl "Logitech Gaming Mouse G600" profile 0 led 0 set mode off
 sleep 0.05
-ratbagctl "Logitech Gaming Mouse G600" profile 0 led 0 set mode on
-sleep 0.05
-
-# ---
-
-ratbagctl "Logitech Gaming Mouse G600" profile 0 led 0 set mode off
-sleep 0.01
 ratbagctl "Logitech Gaming Mouse G600" profile 0 led 0 set color $1
-sleep 0.04
-ratbagctl "Logitech Gaming Mouse G600" profile 0 led 0 set mode on
 sleep 0.05
+ratbagctl "Logitech Gaming Mouse G600" profile 0 led 0 set mode on
