67618697ec7003f63db50f1e3bc7071f108660f0
Dan Habot <dan.habot@gmail.com>
2025-05-04 14:38:22 -0400

    fix bambu studio

--- modules/threedtools/default.nix
+++ modules/threedtools/default.nix
@@ -1,15 +1,23 @@
 import ../module.nix {
-  name =  "threedtools";
+  name = "threedtools";
 
-  output = { pkgs, ... }: {
-    packages = with pkgs; [ 
-      freecad
-      blender
-      bambu-studio
-    ];
-
-    nixos = { };
-
-    homeManager = { };
-  };
+  output = { pkgs, ... }:
+    let
+      bambu-studio = pkgs.bambu-studio.overrideAttrs (oldAttrs: {
+        version = "01.00.01.50";
+        src = pkgs.fetchFromGitHub {
+          owner = "bambulab";
+          repo = "BambuStudio";
+          rev = "v01.00.01.50";
+          hash = "sha256-7mkrPl2CQSfc1lRjl1ilwxdYcK5iRU//QGKmdCicK30=";
+        };
+      });
+    in
+    {
+      packages = [
+        pkgs.freecad
+        pkgs.blender
+        bambu-studio
+      ];
+    };
 }
