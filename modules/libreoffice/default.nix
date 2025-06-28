3a8c72dbed50ec0768079d4f6039a81d6182a354
Dan Habot <dan.habot@gmail.com>
2025-06-28 15:14:05 -0400

    added libreoffice

---
+++ modules/libreoffice/default.nix
@@ -0,0 +1,21 @@
+import ../module.nix
+{
+  name = "libreoffice";
+
+  output = { pkgs, ... }: {
+    packages = with pkgs; [
+      libreoffice-qt
+      hunspell
+      hunspellDicts.uk_UA
+      hunspellDicts.th_TH
+    ];
+
+    homeManager = {
+
+    };
+
+    nixos = {
+
+    };
+  };
+}
