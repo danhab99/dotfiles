d1e07e4147f520e84de315c84deda76fece738ed
Dan Habot <dan.habot@gmail.com>
2025-07-27 00:48:09 -0400

    changes

--- modules/watchdog/default.nix
+++ modules/watchdog/default.nix
@@ -10,23 +10,27 @@ import ../module.nix
     };
 
     nixos = let 
-      timeout = "10";
+      timeout = 10;
+      ts = builtins.toString;
     in {
       boot.kernelParams = [
-        "softlockup_panic=1"
+        "efi_pstore.pstore_disable=0"
         "hardlockup_panic=1"
-        "panic=${timeout}" # reboot 10 seconds after panic
-        "reboot=bios"  # or reboot=acpi
+        "panic=${ts timeout}" # reboot 10 seconds after panic
+        "reboot=bios"
+        "softlockup_panic=1"
       ];
 
       boot.kernelModules = [ "iTCO_wdt" ]; # Intel boards
 
       systemd.watchdog = {
         device = "/dev/watchdog"; # path to hardware watchdog
-        runtimeTime = "${timeout}s"; # system should ping at least this often
-        rebootTime = "${timeout}s"; # if unresponsive for this much time, reboot
+        runtimeTime = "${ts timeout}s"; # system should ping at least this often
+        rebootTime = "${ts ( timeout * 2 ) }s"; # if unresponsive for this much time, reboot
         kexecTime = null; # optional: time to wait during kexec
       };
+
+      boot.crashDump.enable = true;
     };
   };
 }
