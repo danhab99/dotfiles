f166e4dc844490990be9722753c4c1ffeffe44a1
Dan Habot <dan.habot@gmail.com>
2025-07-27 13:56:53 -0400

    removed this

--- modules/obs/default.nix
+++ modules/obs/default.nix
@@ -15,7 +15,6 @@ import ../module.nix
         enable = true;
         plugins = with pkgs.obs-studio-plugins; [
           wlrobs
-          obs-backgroundremoval
           obs-pipewire-audio-capture
         ];
         enableVirtualCamera = true;
