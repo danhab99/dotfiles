128e9e892c1377bcb3f6c01370d20a4401354ca5
Dan Habot <dan.habot@gmail.com>
2025-07-29 21:52:38 -0400

    switch to nixpkgs bambu-studio

--- modules/threedtools/default.nix
+++ modules/threedtools/default.nix
@@ -3,21 +3,21 @@ import ../module.nix {
 
   output = { pkgs, ... }:
     let
-      bambu-studio = pkgs.bambu-studio.overrideAttrs (oldAttrs: {
-        version = "01.00.01.50";
-        src = pkgs.fetchFromGitHub {
-          owner = "bambulab";
-          repo = "BambuStudio";
-          rev = "v01.00.01.50";
-          hash = "sha256-7mkrPl2CQSfc1lRjl1ilwxdYcK5iRU//QGKmdCicK30=";
-        };
-      });
+      # bambu-studio = pkgs.bambu-studio.overrideAttrs (oldAttrs: {
+      #   version = "01.00.01.50";
+      #   src = pkgs.fetchFromGitHub {
+      #     owner = "bambulab";
+      #     repo = "BambuStudio";
+      #     rev = "v01.00.01.50";
+      #     hash = "sha256-7mkrPl2CQSfc1lRjl1ilwxdYcK5iRU//QGKmdCicK30=";
+      #   };
+      # });
     in
     {
       packages = [
         pkgs.freecad
         pkgs.blender
-        bambu-studio
+        pkgs.bambu-studio
       ];
     };
 }
