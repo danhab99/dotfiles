fe5287261dba39d8f50b86960b5a11620d343e1e
Dan Habot <dan.habot@gmail.com>
2025-07-29 21:52:05 -0400

    update flake

--- flake.nix
+++ flake.nix
@@ -3,15 +3,19 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs_for_xpad.url =  "github:nixos/nixpkgs/910796cabe436259a29a72e8d3f5e180fc6dfacc"; 
   };
 
-  outputs = { self, nixpkgs, home-manager, flake-utils, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, flake-utils, nixpkgs_for_xpad, ... }@inputs:
     let
       inherit (self) outputs;
 
       mkNix = hostName:
         nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs outputs; };
+          specialArgs = { 
+            inherit inputs outputs;
+            nixpkgs_for_xpad = import nixpkgs_for_xpad { system = "x86_64-linux"; };
+          };
           modules = [
             ./machine/${hostName}/configuration.nix
             ./machine/${hostName}/hardware-configuration.nix
