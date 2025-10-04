868bc82a96c0cebab1272f1c1076a327335dee2f
Dan Habot <dan.habot@gmail.com>
2025-10-04 12:59:43 -0400

    thinkpad specific audio changes

--- modules/audio/default.nix
+++ modules/audio/default.nix
@@ -4,35 +4,52 @@ import ../module.nix
 
   options = { lib }: with lib; {
     enableBluetooth = lib.mkEnableOption "enableBluetooth";
-    enableJACK = lib.mkEnableOption "enableJACK";
+    # enableJACK = lib.mkEnableOption "enableJACK";
   };
 
   output = { pkgs, cfg, lib, ... }: {
     packages = with pkgs; [
       alsa-utils
       pamixer
+      pavucontrol
+      playerctl
+      pulseaudioFull
     ];
 
     homeManager = { };
 
     nixos = {
+      hardware.bluetooth.enable = cfg.enableBluetooth;
+      hardware.bluetooth.powerOnBoot = cfg.enableBluetooth;
+      services.blueman.enable = cfg.enableBluetooth;
+
+      nixpkgs.config.pulseaudio = true;
+
+      security.rtkit.enable = true;
+
       services.pipewire = {
         enable = true;
         alsa.enable = true;
         alsa.support32Bit = true;
-        pulse.enable = true;    # pipewire-pulse (Pulse clients talk to PipeWire)
-        jack.enable = true;     # optional: let pipewire provide JACK
+        pulse.enable = true;
+
         extraConfig.pipewire = {
           "99-disable-bell" = {
             "context.properties" = { "module.x11.bell" = false; };
           };
         };
+
+        wireplumber = {
+          enable = true;
+          extraConfig.bluetooth = {
+            "monitor.bluez.properties" = {
+              "bluez5.enable-sbc-xq" = true;
+              "bluez5.enable-msbc" = true; # wideband speech for headset mics
+              "bluez5.enable-hw-volume" = true; # lets volume keys sync properly
+            };
+          };
+        };
       };
-
-      hardware.pulseaudio.enable = false;
-      nixpkgs.config.pulseaudio = false;
-
     };
-
   };
 }
