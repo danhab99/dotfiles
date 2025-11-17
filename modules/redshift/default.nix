17e3d3fd208319b9c3b90544f8fd8983041472b3
Dan Habot <dhabot@tradezerofintech.com>
2025-11-17 11:57:13 -0500

    redshift

---
+++ modules/redshift/default.nix
@@ -0,0 +1,38 @@
+import ../module.nix
+{
+  name = "redshift";
+
+  output = { pkgs, ... }: {
+    packages = with pkgs; [
+
+    ];
+
+    homeManager = {
+      services.redshift = {
+        enable = true;
+
+        provider = "geoclue2";
+
+        brightness = {
+          # Note the string values below.
+          day = "1";
+          night = "0.8";
+        };
+        temperature = {
+          # day = 5500;
+          day = 4000;
+          night = 3700;
+        };
+      };
+    };
+
+    nixos = {
+      services.geoclue2 = {
+        enable = true;
+        enableWifi = true;
+        submitData = false;
+      };
+    };
+  };
+}
+
