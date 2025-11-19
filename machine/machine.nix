c390313c49e8653d2eaf8f19f11442ac5a0a1a01
Dan Habot <dan.habot@gmail.com>
2025-11-18 21:34:35 -0500

    remove this

--- machine/machine.nix
+++ machine/machine.nix
@@ -123,10 +123,6 @@ let
         # Before changing this value read the documentation for this option
         # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
         system.stateVersion = "24.05"; # Please read the comment before changing.
-
-        nixpkgs.config.permittedInsecurePackages = [
-          "libsoup-2.74.3"
-        ];
       };
     };
 in
