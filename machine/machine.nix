94811875eb9008e6fe2df8f27ad1e2924dfaa206
Dan Habot <dan.habot@gmail.com>
2025-11-22 15:15:03 -0500

    build uconsole

--- machine/machine.nix
+++ machine/machine.nix
@@ -10,6 +10,7 @@
 , bind ? [ ]
 , jobs ? (args: [ ])
 , nixos ? { }
+, extraNixosModules ? { ... }: [ ]
 }:
 let
   strLen = builtins.stringLength;
@@ -118,15 +119,15 @@ let
 
         # This value determines the NixOS release from which the default
         # settings for stateful data, like file locations and database versions
-        # on your system were taken. It‘s perfectly fine and recommended to leave
+        # on your system were taken. It's perfectly fine and recommended to leave
         # this value at the release version of the first install of this system.
         # Before changing this value read the documentation for this option
         # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-        system.stateVersion = "24.05"; # Please read the comment before changing.
+        system.stateVersion = lib.mkDefault "24.05"; # Please read the comment before changing.
       };
     };
 in
-inputs@{ nixpkgs, home-manager, nixpkgs_for_xpad, ... }: 
+inputs@{ nixpkgs, home-manager, nixpkgs_for_xpad, ... }:
 nixpkgs.lib.nixosSystem {
   inherit system;
   specialArgs = {
@@ -141,6 +142,5 @@ nixpkgs.lib.nixosSystem {
       home-manager.useGlobalPkgs = true;
       home-manager.useUserPackages = true;
     }
-    nixos
-  ];
+  ] ++ (extraNixosModules inputs);
 }
