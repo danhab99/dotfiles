0a39751c752134214625543f5353fc6db7eb6989
Dan Habot <dan.habot@gmail.com>
2025-12-20 23:24:53 -0500

    finally have a dedicated lock button

--- modules/i3/config.nix
+++ modules/i3/config.nix
@@ -112,8 +112,10 @@ in
     };
   }];
 
-  keybindings = {
-    "Pause" = "exec /home/dan/.config/i3/lock.sh";
+  keybindings = let
+    lockScreenExe = "exec ${pkgs.betterlockscreen}/bin/betterlockscreen --lock -- fork";
+  in {
+    "Pause" = lockScreenExe;
     "XF86AudioLowerVolume" = "exec amixer sset Master 5%-";
     "XF86AudioRaiseVolume" = "exec amixer sset Master 5%+";
     "XF86AudioMute" = "exec amixer sset Master toggle";
@@ -212,10 +214,7 @@ in
     "${mod}+d" = "exec $HOME/.config/rofi/scripts/launcher_t1";
     "${mod}+Tab" =
       "exec $HOME/.config/rofi/launchers/misc/launch_windows.sh";
-    "${mod}+shift+x" = "exec ${pkgs.betterlockscreen}/bin/betterlockscreen --lock -- fork";
-    "Caps_L" = "exec --no-startup-id rofi -show drun";
-    "Caps_Lock" =
-      "exec --no-startup-id exec xdotool key Caps_Lock $HOME/.config/rofi/scripts/launcher_t1";
+    "${mod}+shift+x" = lockScreenExe;
   };
 
   workspaceOutputAssign =
