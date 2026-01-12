c496aded0ab796b585f2c722f674c4eadf731f07
Dan Habot <dan.habot@gmail.com>
2026-01-12 11:39:33 -0500

    home.stateVersion update

--- users/user.nix
+++ users/user.nix
@@ -43,7 +43,8 @@ in
         # You should not change this value, even if you update Home Manager. If you do
         # want to update the value, then make sure to first check the Home Manager
         # release notes.
-        home.stateVersion = "24.05"; # Please read the comment before changing.
+        # home.stateVersion = "24.05"; # Please read the comment before changing.
+        home.stateVersion = "26.05"; # Please read the comment before changing.
       };
     };
 }
