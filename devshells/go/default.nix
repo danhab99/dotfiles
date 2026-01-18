0ee6174ed41db6e616eb5474e3ece54ce7ad8ca7
Dan Habot <dan.habot@gmail.com>
2026-01-17 22:51:48 -0500

    created new go devshell

---
+++ devshells/go/default.nix
@@ -0,0 +1,18 @@
+
+import ../devshell.nix
+{
+  name = "go";
+
+  versions = { self, pkgs, ... }: {
+    "" = {
+      packages = with pkgs; [
+        go
+        gopls
+      ];
+
+      env = {
+        GO_PATH="${self.outPath}/.go";
+      };
+    };
+  };
+}
