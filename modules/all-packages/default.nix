79a1538d644223827585e87ba6873eca678cdbea
Dan Habot <dan.habot@gmail.com>
2026-02-07 14:35:25 -0500

    commands

--- modules/all-packages/default.nix
+++ modules/all-packages/default.nix
@@ -7,12 +7,15 @@ import ../module.nix {
       packages = with pkgs; [
         glances
         just
+        killall
+        nixfmt-tree
         nodejs
         openssl
         python3
         s3cmd
         scdl
         sshfs
+        unzip
         yai
         yt-dlp
         zip
