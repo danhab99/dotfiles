c01108227b122c9862587ad10d2c343d32132701
Dan Habot <dan.habot@gmail.com>
2026-02-08 07:34:54 +0000

    broke off changelog template

---
+++ templates/changelog/files/flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachSystem flake-utils.lib.defaultSystems (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        lib = pkgs.lib;
+
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              gnumake
+              # ...
+            ];
+
+            shellHook = ''
+              zsh
+            '';
+          };
+        };
+      }
+    );
+}
