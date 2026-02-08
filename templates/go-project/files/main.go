31fb76de986e1c19da6ea877123a4edda68ea56f
Dan Habot <dan.habot@gmail.com>
2026-02-08 07:35:24 +0000

    stripped

--- templates/go-project/files/main.go
+++ templates/go-project/files/main.go
@@ -1,13 +1,6 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"os"
-
-	"grit/cmd/export"
-	"grit/cmd/run"
-	"grit/cmd/status"
 )
 
 func main() {
@@ -19,23 +12,11 @@ func main() {
 	command := os.Args[1]
 
 	switch command {
-	case "run":
-		runCmd := flag.NewFlagSet("run", flag.ExitOnError)
-		run.RegisterFlags(runCmd)
-		runCmd.Parse(os.Args[2:])
-		run.Execute()
-
-	case "export":
-		exportCmd := flag.NewFlagSet("export", flag.ExitOnError)
-		export.RegisterFlags(exportCmd)
-		exportCmd.Parse(os.Args[2:])
-		export.Execute()
-
-	case "status":
-		statusCmd := flag.NewFlagSet("status", flag.ExitOnError)
-		status.RegisterFlags(statusCmd)
-		statusCmd.Parse(os.Args[2:])
-		status.Execute()
+	// case "run":
+	// 	runCmd := flag.NewFlagSet("run", flag.ExitOnError)
+	// 	run.RegisterFlags(runCmd)
+	// 	runCmd.Parse(os.Args[2:])
+	// 	run.Execute()
 
 	case "help", "-h", "--help":
 		printUsage()
@@ -48,12 +29,4 @@ func main() {
 }
 
 func printUsage() {
-	fmt.Println("grit - Task pipeline management system")
-	fmt.Println("\nUsage:")
-	fmt.Println("  grit <command> [flags]")
-	fmt.Println("\nAvailable commands:")
-	fmt.Println("  run       Run the pipeline")
-	fmt.Println("  export    Export resources from the database")
-	fmt.Println("  status    Show pipeline status and statistics")
-	fmt.Println("\nUse 'grit <command> -h' for more information about a command.")
 }
