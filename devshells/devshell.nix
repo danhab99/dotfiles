7f1ce5ed6ae77f202c9a010393839ed43be48d8d
Dan Habot <dan.habot@gmail.com>
2026-03-03 17:30:18 -0500

    devshells now avaliable as templates

--- devshells/devshell.nix
+++ devshells/devshell.nix
@@ -1,16 +1,77 @@
-{
-  name,
-  versions,
+{ name
+, versions
+,
 }:
 inputs@{ pkgs, lib, ... }:
-lib.attrsets.mapAttrs (
-  version: body:
-  pkgs.mkShell (
-    body.env or { }
-    // {
-      name = "${name}-${version}";
-      buildInputs = body.packages;
-      shellHook = body.shellHook or "";
+lib.attrsets.mapAttrs
+  (
+    version: body:
+    {
+      shell = pkgs.mkShell
+        (
+          body.env or { }
+          // {
+            name = "${name}-${version}";
+            buildInputs = body.packages;
+            shellHook = body.shellHook or "";
+          }
+        );
+
+      template =
+        let
+          packages = map (pkg: pkg.pname) body.packages;
+
+        in
+        {
+          description = "${name}-${version} template";
+          path = pkgs.stdenv.mkDerivation
+            {
+              pname = "devshell-template-${name}-${version}";
+              inherit version;
+
+              dontUnpack = true;
+
+              installPhase = ''
+                cat > $out/flake.nix <<EOF
+                {
+                  description = "${name}-${version} template";
+                  inputs = {
+                    nixpkgs.url = "github:NixOS/nixpkgs";
+                    flake-utils.url = "github:numtide/flake-utils";
+                  };
+
+                  outputs =
+                    {
+                      self,
+                      nixpkgs,
+                      flake-utils,
+                    }:
+                    flake-utils.lib.eachSystem flake-utils.lib.defaultSystems (
+                      system:
+                      let
+                        pkgs = import nixpkgs {
+                          inherit system;
+                          config.allowUnfree = true;
+                        };
+                        lib = pkgs.lib;
+
+                      in
+                      {
+                        devShells = {
+                          default = pkgs.mkShell {
+                            packages = with pkgs; [
+                              just
+                              ${builtins.concatStringsSep "\n" packages}
+                            ];
+                          };
+                        };
+                      }
+                    );
+                }
+                EOF
+              '';
+            };
+        };
     }
   )
-) (versions inputs)
+  (versions inputs)
