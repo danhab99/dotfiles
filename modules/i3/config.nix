491e41936e4726595b4a7dc61be28f0f6d12be1e
Dan Habot <dan.habot@gmail.com>
2026-03-07 01:34:51 -0500

    rofi

--- modules/i3/config.nix
+++ modules/i3/config.nix
@@ -218,7 +218,7 @@ in
       "${mod}+Shift+e" =
         "exec i3-nagbar -t warning -m 'You pressed the exit shortcut. Do you really want to exit i3? This will end your X session.' -B 'Yes, exit i3' 'i3-msg exit'";
       "${mod}+r" = "mode resize";
-      "${mod}+d" = "exec i3-dmenu-desktop";
+      "${mod}+d" = "exec app-launcher";
       "${mod}+shift+x" = lockScreenExe;
     };
 
