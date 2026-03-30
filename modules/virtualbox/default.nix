67115f8697b5607a16f132bbb67e51e689c902dd
Dan Habot <dhabot@tradezerofintech.com>
2026-03-30 16:14:48 -0400

    ignoring too many things

---
+++ modules/virtualbox/default.nix
@@ -0,0 +1,21 @@
+import ../module.nix
+{
+  name = "virtualbox";
+
+  output = { pkgs, ... }: {
+    packages = with pkgs; [
+
+    ];
+
+    homeManager = {
+
+    };
+
+    nixos = {
+      virtualisation.virtualbox = {
+        host.enable = true;
+      };
+    };
+  };
+}
+
