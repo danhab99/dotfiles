7a49586aec9ead3b1647a3ace962aef7d5fa86a3
Dan Habot <dan.habot@gmail.com>
2026-04-11 22:52:22 -0400

    switch to pipewire

--- modules/audio/default.nix
+++ modules/audio/default.nix
@@ -27,29 +27,11 @@ import ../_module.nix {
       homeManager = { };
 
       nixos = {
-        hardware = {
-          alsa.enable = true;
-          alsa.enablePersistence = true;
-          bluetooth.enable = cfg.enableBluetooth;
-          bluetooth.powerOnBoot = cfg.enableBluetooth;
-        };
-
         security.rtkit.enable = true;
 
-        services = {
-          blueman.enable = cfg.enableBluetooth;
-          pipewire.enable = lib.mkForce false;
-          pipewire.wireplumber.enable = lib.mkForce false;
-          pulseaudio = {
-            enable = lib.mkForce true; # current option
-            package = pkgs.pulseaudioFull; # codecs, BT
-            support32Bit = true; # Steam/Wine
-
-            daemon.config = {
-              default-sample-rate = 48000; # Or 44100
-              alternate-sample-rate = 44100; # Recommended to have both
-            };
-          };
+        services.pipewire = {
+          enable = true;
+          audio.enable = true;
         };
       };
     };
