6564f0547e36c11dbb9f653726f8c48a2e817dc6
Dan Habot <dan.habot@gmail.com>
2026-04-19 19:49:47 -0400

    i3 like bindings for tmux

--- modules/tmux/default.nix
+++ modules/tmux/default.nix
@@ -20,6 +20,70 @@ import ../_module.nix {
           # Note: the `attach` is redundant since new-session attaches, but included for clarity
         fi
       '';
+      workspaceBindings =
+        let
+          workspaces = [
+            {
+              key = "1";
+              shiftKey = "!";
+              target = "1";
+            }
+            {
+              key = "2";
+              shiftKey = "@";
+              target = "2";
+            }
+            {
+              key = "3";
+              shiftKey = "#";
+              target = "3";
+            }
+            {
+              key = "4";
+              shiftKey = "$";
+              target = "4";
+            }
+            {
+              key = "5";
+              shiftKey = "%";
+              target = "5";
+            }
+            {
+              key = "6";
+              shiftKey = "^";
+              target = "6";
+            }
+            {
+              key = "7";
+              shiftKey = "&";
+              target = "7";
+            }
+            {
+              key = "8";
+              shiftKey = "*";
+              target = "8";
+            }
+            {
+              key = "9";
+              shiftKey = "(";
+              target = "9";
+            }
+            {
+              key = "0";
+              shiftKey = ")";
+              target = "10";
+            }
+          ];
+        in
+        lib.concatMapStringsSep "\n" (
+          { key, shiftKey, target }:
+          ''
+            bind-key ${key} select-window -t :=${target}
+            bind-key -n A-${key} select-window -t :=${target}
+            bind-key ${shiftKey} swap-window -t :=${target}
+            bind-key -n A-${shiftKey} swap-window -t :=${target}
+          ''
+        ) workspaces;
     in
     {
       packages = with pkgs; [
@@ -37,10 +101,19 @@ import ../_module.nix {
           clock24 = true;
           keyMode = "vi";
           newSession = false;
+          prefix = "C-a";
 
           terminal = "$TERM";
 
           extraConfig = ''
+            unbind-key C-b
+            bind-key C-a send-prefix
+            set -sg escape-time 0
+            set -g pane-base-index 1
+            set -g renumber-windows on
+            set -g status-position bottom
+            set -g status-justify left
+
             # Set key bindings for copy mode
             setw -g mode-keys vi
             bind-key -T copy-mode-vi r send -X rectangle-toggle
@@ -50,6 +123,17 @@ import ../_module.nix {
 
             # Enable mouse support
             set -g mouse on
+
+            # Mirror the i3 palette so tmux windows feel like workspaces.
+            set -g status-style bg=#2f343f,fg=#f3f4f5
+            set -g message-style bg=#2f343f,fg=#f3f4f5
+            set -g message-command-style bg=#2f343f,fg=#f3f4f5
+            set -g pane-border-style fg=#676E7D
+            set -g pane-active-border-style fg=#f3f4f5
+            setw -g window-status-format " #I "
+            setw -g window-status-style bg=default,fg=#676E7D
+            setw -g window-status-current-format " #I "
+            setw -g window-status-current-style bg=#2f343f,fg=#f3f4f5
 
             # Set environment variable for DISPLAY
             set-environment -g DISPLAY :0
@@ -64,7 +148,53 @@ import ../_module.nix {
             set-option -g set-titles off
             set-option -g automatic-rename off
 
-            set-option -g status off
+            # i3-style pane and workspace navigation, using Alt as tmux's mod key.
+            bind-key h select-pane -L
+            bind-key j select-pane -D
+            bind-key k select-pane -U
+            bind-key l select-pane -R
+            bind-key -n A-h select-pane -L
+            bind-key -n A-j select-pane -D
+            bind-key -n A-k select-pane -U
+            bind-key -n A-l select-pane -R
+
+            bind-key H swap-pane -t '{left-of}'
+            bind-key J swap-pane -t '{down-of}'
+            bind-key K swap-pane -t '{up-of}'
+            bind-key L swap-pane -t '{right-of}'
+            bind-key -n A-H swap-pane -t '{left-of}'
+            bind-key -n A-J swap-pane -t '{down-of}'
+            bind-key -n A-K swap-pane -t '{up-of}'
+            bind-key -n A-L swap-pane -t '{right-of}'
+
+            bind-key v split-window -h -c '#{pane_current_path}'
+            bind-key V split-window -v -c '#{pane_current_path}'
+            bind-key -n A-v split-window -h -c '#{pane_current_path}'
+            bind-key -n A-V split-window -v -c '#{pane_current_path}'
+            bind-key Enter new-window -c '#{pane_current_path}'
+
+            bind-key q kill-pane
+            bind-key -n A-q kill-pane
+            bind-key f resize-pane -Z
+            bind-key -n A-f resize-pane -Z
+            bind-key Tab last-window
+            bind-key -n A-Tab last-window
+
+            bind-key r switch-client -T resize
+            bind-key -n A-r switch-client -T resize
+            bind-key -T resize h resize-pane -L 5
+            bind-key -T resize j resize-pane -D 5
+            bind-key -T resize k resize-pane -U 5
+            bind-key -T resize l resize-pane -R 5
+            bind-key -T resize Left resize-pane -L 5
+            bind-key -T resize Down resize-pane -D 5
+            bind-key -T resize Up resize-pane -U 5
+            bind-key -T resize Right resize-pane -R 5
+            bind-key -T resize Enter switch-client -T root
+            bind-key -T resize Escape switch-client -T root
+            bind-key -T resize r switch-client -T root
+
+            ${workspaceBindings}
           '';
         };
 
