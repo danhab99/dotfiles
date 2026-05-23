b40a7e97f5b9505db34b7bf0f435a6868925a10b
Dan Habot <dan.habot@gmail.com>
2026-05-22 23:56:44 -0400

    install duh

--- modules/my-packages/default.nix
+++ modules/my-packages/default.nix
@@ -3,11 +3,13 @@ import ../_module.nix
   name = "my-packages";
   requires = {
     # grit.url = "github:danhab99/grit/main";
+    duh.url = "github:danhab99/duh/main";
   };
 
-  output = { pkgs, ... }: {
+  output = { pkgs, duh, ... }: {
     packages = with pkgs; [
       # grit.packages.x86_64-linux.default
+      duh.packages.x86_64-linux.duh
     ];
 
   };
