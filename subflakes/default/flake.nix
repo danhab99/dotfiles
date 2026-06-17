415b4d9aeb24d358f5bc1533c1f08fca457a8b16
Dan Habot <dan.habot@gmail.com>
2026-06-17 11:09:43 -0400

    pin

--- subflakes/default/flake.nix
+++ subflakes/default/flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-cli.url = "github:nix-community/nixos-cli/main";
